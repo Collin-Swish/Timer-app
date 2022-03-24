e0524f189e4c5c4d0b023810b3404ba36bd931f4
Collin Swisher <collinswish1@gmail.com>
2022-03-23 19:09:02 -0900

    Stable version

--- src/script.js
+++ src/script.js
@@ -1,12 +1,32 @@
 const fs = require("fs");
-const Stopwatch = require("stopwatch")
+var startTime;
+var interval;
+
+// Converts Date objects to a { hour : minute : second } format
+function timeToString(time){
+    // Full disclosure I copied this code from w3schools
+    var hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((time % (1000 * 60)) / 1000);
+    return `${hours} : ${minutes} : ${seconds}`
+}
 
 
 
 window.addEventListener("DOMContentLoaded",()=>{
-    console.log("finished")
-    const button = document.querySelector("button")
-    button.addEventListener("click",()=>{
-        fs.mkdir("hello",()=>{})
+    let start = document.querySelector("button")
+    start.addEventListener("click",()=>{
+        startTime = Date.now();
+        interval = setInterval(()=>{
+            updateCounter(timeToString(Date.now() - startTime));
+        });
+    })
+    document.querySelector("#stop").addEventListener("click",()=>{
+        clearInterval(interval);
     })
 })
+
+// Updates the paragraph element holding the timer display
+function updateCounter(content){
+    document.querySelector("#counter").innerHTML = content
+}
